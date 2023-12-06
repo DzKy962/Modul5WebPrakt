0f0dab7b3cac286813ac70188e276745f75cfafb
DzKy962 <dzakyfadhilla962@gmail.com>
2023-12-06 15:52:34 +0700

    commit

---
+++ api/Routes/ProductRoutes.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace api\Routes;
+
+include "api/Controller/ProductController.php";
+
+use api\Controller\ProductController;
+
+class ProductRoutes
+{
+  public function handle($method, $path)
+  {
+    if ($method === 'GET' && $path === '/api/product/') {
+      $contoller = new ProductController();
+      echo $contoller->index();
+    }
+    if ($method === 'GET' && strpos($path, '/api/product/') === 0) {
+      $pathParts = explode('/', $path);
+      $id = $pathParts[count($pathParts) - 1];
+
+      $contoller = new ProductController();
+      echo $contoller->getById($id);
+    }
+    if ($method === 'POST' && $path === '/api/product/') {
+      $contoller = new ProductController();
+      echo $contoller->insert();
+    }
+    if ($method === 'PUT' && strpos($path, '/api/product/') === 0) {
+      $pathParts = explode('/', $path);
+      $id = $pathParts[count($pathParts) - 1];
+
+      $contoller = new ProductController();
+      echo $contoller->update($id);
+    }
+    if ($method === 'DELETE' && strpos($path, '/api/product/') === 0) {
+      $pathParts = explode('/', $path);
+      $id = $pathParts[count($pathParts) - 1];
+
+      $contoller = new ProductController();
+      echo $contoller->delele($id);
+    }
+  }
+}
